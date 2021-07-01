c75527671cc57566d2ef2940596e2b3d03773ec3
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-07-01 19:25:14 -0300

    Add notification intent

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -41,8 +41,15 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // Create the content intent for the notification, which launches
     // this activity
     // TODO: Step 1.11 create intent
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
 
     // TODO: Step 1.12 create PendingIntent
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     // TODO: Step 2.0 add style
 
@@ -58,6 +65,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 
     // TODO: Step 1.3 set title, text and icon to builder
     builder
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
         .setSmallIcon(R.drawable.cooked_egg)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
